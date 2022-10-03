94ed4af6b054859f518232c91eda5e453662714c
Patsch03 <pschenk0304@gmail.com>
2022-10-03 19:43:44 -0400

    initial commit

---
+++ routes/api/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../../controllers/api/users');
+const ensureLoggedIn = require('../../controllers/api/users')
+
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
+// POST : http://localhost:3001/api/users
+router.post('/', usersCtrl.create);
+
+// POST : http://localhost:3001/api/users/login
+router.post('/login', usersCtrl.login);
+
+module.exports = router;
